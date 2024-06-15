ce9f17f66453c3f634a33a87e32d724d1611efc0
Echo5414 <105505082+Echo5414@users.noreply.github.com>
2024-06-15 18:25:41 +0200

    fixed bugs

--- src/lib/custom-jsx.d.ts
+++ src/lib/custom-jsx.d.ts
@@ -1,6 +1,6 @@
+// custom-jsx.d.ts
 declare namespace svelteHTML {
   interface HTMLAttributes<T> {
-    // Add your custom attributes here
     "app-p-xs"?: string;
     // Add other custom attributes as needed
   }
